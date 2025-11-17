03cddcb86fc1429d15d9b9f173c4ac1f62af87b0
elzahraku <malikelza9@gmail.com>
2025-11-17 14:36:14 +0700

    Update user.py

--- pyromod/listen/user.py
+++ pyromod/listen/user.py
@@ -1,21 +1,23 @@
-import pyrogram
+import hydrogram as pyrogram  # alias hydrogram -> pyrogram supaya sisa kode tetap kompatibel
 
 from .client import Client
 from ..utils import patch_into, should_patch
 
 
 @patch_into(pyrogram.types.user_and_chats.user.User)
-class User(pyrogram.types.user_and_chats.user.User):
+class User:
     _client: Client
 
     @should_patch()
     def listen(self, *args, **kwargs):
-        return self._client.listen(*args, user_id=self.id, **kwargs)
+        return self._client.listen(*args, user_id=getattr(self, "id", None), **kwargs)
 
     @should_patch()
     def ask(self, text, *args, **kwargs):
-        return self._client.ask(self.id, text, *args, user_id=self.id, **kwargs)
+        return self._client.ask(
+            getattr(self, "id", None), text, *args, user_id=getattr(self, "id", None), **kwargs
+        )
 
     @should_patch()
     def stop_listening(self, *args, **kwargs):
-        return self._client.stop_listening(*args, user_id=self.id, **kwargs)
+        return self._client.stop_listening(*args, user_id=getattr(self, "id", None), **kwargs)
